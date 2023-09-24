fdef713a168cdeb10b94b3ae1ff0c67665c94520
tienanh0903k <tienanh2003k@gmail.com>
2023-09-24 22:04:51 +0700

    no

---
+++ DataModel/TaiKhoanModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DataModel
+{
+    public class TaiKhoanModel
+    {
+        public int MaTK { get; set; }
+        public string LoaiTK { get; set; }
+        public string TenTK { get; set; }
+        public string MatKhau { get; set; }
+        public string Email { get; set; }
+    }
+}
