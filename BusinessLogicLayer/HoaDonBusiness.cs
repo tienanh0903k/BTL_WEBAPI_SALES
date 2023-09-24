faa44965ba5835af39d2959e4516e141b8914f67
tienanh0903k <tienanh2003k@gmail.com>
2023-09-24 21:45:17 +0700

    Add project files.

---
+++ BusinessLogicLayer/HoaDonBusiness.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using DataModel;
+
+namespace BusinessLogicLayer
+{
+    public class HoaDonBusiness:IHoaDonBusiness
+    {
+        private IHoaDonRepository _res;
+        public HoaDonBusiness(IHoaDonRepository res)
+        {
+            _res = res;
+        }
+        public bool Create(HoaDonModel model)
+        {
+            return _res.Create(model);
+        }
+        
+    }
+}
